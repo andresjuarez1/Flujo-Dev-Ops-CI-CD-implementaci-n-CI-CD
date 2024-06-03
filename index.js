6c2ae1653cb17b7efc97606caee93f84ce834914
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2024-06-03 17:25:40 -0600

    prueba

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hola mundo a todos');
+  res.send('Hola mundo a todos desde master');
 });
 
 app.listen(port, () => {
