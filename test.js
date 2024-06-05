3947827d0ea13f87d6c233d5e20e453926912f10
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2024-06-04 22:52:05 -0600

    practice 3

---
+++ test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.get('/api/v1/welcome', (req, res) => {
+  res.status(200).send('Hello, World!');
+});
+
+describe('GET /api/v1/welcome', () => {
+  it('respond with Hello, World!', (done) => {
+    request(app)
+      .get('/api/v1/welcome')
+      .expect(200)
+      .expect('Hello, World!', done);
+  });
+});
